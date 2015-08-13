cc49fd966d3ac1fcf1c1f7d08ba70d08a4d8b42e
taocaocajaini <278233503@qq.cpm>
2015-08-13 12:06:39 +0800

    添加loading 界面

--- app/src/main/java/com/cdhxqh/household_app/ui/actvity/BaseActivity.java
+++ app/src/main/java/com/cdhxqh/household_app/ui/actvity/BaseActivity.java
@@ -6,13 +6,22 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 import com.cdhxqh.household_app.R;
+import com.cdhxqh.household_app.app.AppManager;
 
 public class BaseActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //添加Activity到堆栈
+        AppManager.getAppManager().addActivity(this);
     }
 
-
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        //结束Activity&从堆栈中移除
+        AppManager.getAppManager().finishActivity(this);
+    }
 }
---
+++ app/src/main/java/com/cdhxqh/household_app/ui/actvity/Load_Activity.java
@@ -0,0 +1,76 @@
+package com.cdhxqh.household_app.ui.actvity;
+
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.SystemClock;
+import android.widget.ImageView;
+
+import com.cdhxqh.household_app.R;
+
+import java.util.Random;
+
+/**
+ * Entry Activity
+ *
+ * @author bxbxbai
+ */
+public class Load_Activity extends BaseActivity {
+
+    private static final int ANIMATION_DURATION = 2000;
+    private static final float SCALE_END = 1.13F;
+
+
+    private static final int[] SPLASH_ARRAY = {
+            R.drawable.front_cover,
+
+    };
+
+    ImageView mSplashImage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_load_);
+        mSplashImage=(ImageView)findViewById(R.id.iv_entry);
+        mSplashImage.setImageResource(R.drawable.front_cover);
+        animateImage();
+    }
+
+
+    private void animateImage() {
+        ObjectAnimator animatorX = ObjectAnimator.ofFloat(mSplashImage, "scaleX", 1f, SCALE_END);
+        ObjectAnimator animatorY = ObjectAnimator.ofFloat(mSplashImage, "scaleY", 1f, SCALE_END);
+
+        AnimatorSet set = new AnimatorSet();
+        set.setDuration(ANIMATION_DURATION).play(animatorX).with(animatorY);
+        set.start();
+
+        set.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                Intent intent = new Intent();
+                intent.setClass(Load_Activity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+}
