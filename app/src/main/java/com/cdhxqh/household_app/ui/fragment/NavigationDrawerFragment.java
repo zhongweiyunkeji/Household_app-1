fc64e5cfd6b1e35be300029047cae4b45a7d26ce
hexian <1336125942@qq.com>
2015-08-13 11:03:15 +0800

    测试

--- app/src/main/java/com/cdhxqh/household_app/ui/fragment/NavigationDrawerFragment.java
+++ app/src/main/java/com/cdhxqh/household_app/ui/fragment/NavigationDrawerFragment.java
@@ -295,7 +295,7 @@ public class NavigationDrawerFragment extends BaseFragment {
         android.support.v7.app.ActionBar actionBar = getActionBarActivity().getSupportActionBar();
         actionBar.setDisplayShowTitleEnabled(true);
         actionBar.setDisplayShowCustomEnabled(false);
-        Log.e("AAA", "BBB");
+        // Log.e("AAA", "BBB");
     }
 
     /**
