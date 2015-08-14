c21dc9d8c42f58f291352f8240748e4ea0dc21ae
hexian <1336125942@qq.com>
2015-08-14 14:04:14 +0800

    测试

---
+++ app/src/main/java/com/cdhxqh/household_app/ui/adapter/HelpCenterExpandableListViewAdapter.java
@@ -0,0 +1,177 @@
+package com.cdhxqh.household_app.ui.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseExpandableListAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.cdhxqh.household_app.R;
+import com.cdhxqh.household_app.ui.actvity.Activity_Help_Center;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by hexian on 2015/8/12.
+ */
+public class HelpCenterExpandableListViewAdapter extends BaseExpandableListAdapter {
+
+    private Context context;
+    private ArrayList<String> group = new ArrayList<String>(0);
+    private Map<String, List<String>> child = new HashMap<String, List<String>>(0);
+
+    Activity_Help_Center.ItemClickListener itemClickListener;
+
+    public HelpCenterExpandableListViewAdapter(Context context, Activity_Help_Center.ItemClickListener itemClickListener) {
+        this.context = context;
+        this.itemClickListener = itemClickListener;
+        if(itemClickListener == null){
+            throw new RuntimeException("二级节点的监听器不能为空");
+        }
+    }
+
+    @Override
+    public Object getChild(int groupPosition, int childPosition) {
+        return child.get(group.get(groupPosition)).get(childPosition);
+    }
+
+    @Override
+    public int getGroupCount() {
+        return group.size();
+    }
+
+    @Override
+    public int getChildrenCount(int groupPosition) {
+        return child.get(group.get(groupPosition)).size();
+    }
+
+    @Override
+    public Object getGroup(int groupPosition) {
+        return group.get(groupPosition);
+    }
+
+    @Override
+    public long getGroupId(int groupPosition) {
+        return groupPosition;
+    }
+
+    @Override
+    public long getChildId(int groupPosition, int childPosition) {
+        return childPosition;
+    }
+
+    @Override
+    public boolean hasStableIds() {
+        return false;
+    }
+
+    @Override
+    public boolean isChildSelectable(int groupPosition, int childPosition) {
+        return false;
+    }
+
+    @Override
+    public View getGroupView(int groupPosition, boolean isExpanded, View convertView, ViewGroup parent) {
+        ViewHolderGroup holder = null;
+        if(convertView==null){
+            convertView =  LayoutInflater.from(context).inflate(R.layout.help_center_adapter_group, null);
+            holder = new ViewHolderGroup(convertView);
+            convertView.setTag(holder);
+        } else {
+            holder = (ViewHolderGroup)convertView.getTag();
+        }
+
+        holder.textView.setText(group.get(groupPosition));
+        holder.imgView.setImageResource(R.drawable.ic_cameras_blue);
+        convertView.setClickable(true);  // 设置点击事件无效
+        // ImageLoader.getInstance().displayImage("http://www.baidu.com/abc.png", holder.imgView);
+
+        return convertView;
+    }
+
+    @Override
+    public View getChildView(int groupPosition, int childPosition, boolean isLastChild, View convertView, final ViewGroup parent) {
+        ViewHolderChild holder = null;
+        if(convertView==null){
+            convertView =  LayoutInflater.from(context).inflate(R.layout.help_center_adapter_item, null);
+            holder = new ViewHolderChild(convertView);
+            convertView.setTag(holder);
+        } else {
+            holder = (ViewHolderChild)convertView.getTag();
+        }
+
+        holder.imgView.setImageResource(R.drawable.ic_cameras_blue);
+        holder.textView.setText(child.get(group.get(groupPosition)).get(childPosition));
+
+        final View view = convertView;
+        final ImageView imageView= holder.imgView;
+        final int groupIndex = groupPosition;
+        final int childIndex = childPosition;
+        final boolean hasLastChild = !isLastChild;
+        final   ImageView rightView =  holder.rightView;
+        holder.rightView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(itemClickListener != null){
+                    itemClickListener.onItemClick(parent, view, rightView, groupIndex, childIndex, hasLastChild);
+                }
+            }
+        });
+
+        return convertView;
+    }
+
+    public void update(List<String> glist, Map<String, List<String>> ilist){
+        for(String order : glist){
+            boolean flag = true;
+            for(String module : group){
+                if(order.equals(module)){
+                    flag = false;
+                    break;
+                }
+            }
+            if(flag){
+                group.add(order);
+                child.put(order, ilist.get(order));
+            }
+        }
+
+        notifyDataSetChanged();
+    }
+
+    public ArrayList<String> getGroup() {
+        return group;
+    }
+
+    public static final class ViewHolderGroup {
+
+        ImageView imgView;
+        TextView textView;
+
+        public ViewHolderGroup(View view){
+            imgView = (ImageView)view.findViewById(R.id.group_img);
+            textView = (TextView)view.findViewById(R.id.group_text);
+        }
+
+    }
+
+    public static final class ViewHolderChild {
+
+        ImageView imgView;
+        TextView textView;
+        ImageView rightView;
+
+        public ViewHolderChild(View view){
+            imgView = (ImageView)view.findViewById(R.id.item_img);
+            textView = (TextView)view.findViewById(R.id.item_text);
+            rightView = (ImageView)view.findViewById(R.id.item_right);
+        }
+
+    }
+
+}
---
+++ app/src/main/java/com/cdhxqh/household_app/ui/adapter/UserSelectAdapter.java
@@ -0,0 +1,112 @@
+package com.cdhxqh.household_app.ui.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.CompoundButton;
+import android.widget.RadioButton;
+import android.widget.TextView;
+import com.cdhxqh.household_app.R;
+import com.cdhxqh.household_app.ui.action.OnItemClickCallBack;
+import java.util.ArrayList;
+
+/**
+ * Created by hexian on 2015/8/10.
+ */
+public class UserSelectAdapter extends BaseAdapter {
+
+    private Context context;
+    private ArrayList<String> list = new ArrayList<String>(0);
+    private int index = -1;  // 记录当前选中的单选按钮
+    OnItemClickCallBack callBack;
+
+
+    public UserSelectAdapter(Context context, ArrayList<String> list, OnItemClickCallBack callBack){
+        this.context = context;
+        this.list = list;
+        this.callBack = callBack;
+    }
+
+    @Override
+    public int getCount() {
+        return list.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return list.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(final int position, View convertView, ViewGroup parent) {
+        ItemHolder holder = null;
+        if(convertView == null){
+            convertView =  LayoutInflater.from(context).inflate(R.layout.select_usertype_adapter_item, null);
+            holder = new ItemHolder(convertView);
+            convertView.setTag(holder);
+        } else {
+            holder = (ItemHolder)convertView.getTag();
+        }
+
+        holder.textView.setText(list.get(position));
+
+
+        convertView.setOnClickListener(new View.OnClickListener() {  // 注册监听事件
+            @Override
+            public void onClick(View v) {
+                if (callBack != null) {
+                    callBack.onClick(UserSelectAdapter.this, v, position);
+                }
+            }
+        });
+
+        final View v = convertView;
+        holder.radioButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {// 注册单选按钮变更事件
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (callBack != null) {
+                    callBack.onClick(UserSelectAdapter.this, v, position);
+                }
+            }
+        });
+
+        if(index == position){// 关键代码
+            holder.radioButton.setChecked(true);
+        } else {// 关键代码
+            holder.radioButton.setChecked(false);
+        }
+
+
+        return convertView;
+    }
+
+    public static class ItemHolder {
+        TextView textView;
+        RadioButton radioButton;
+
+        public ItemHolder(View view){
+            textView = (TextView)view.findViewById(R.id.select_item_text);
+            radioButton = (RadioButton)view.findViewById(R.id.select_item_radiobutton);
+        }
+
+    }
+
+    public void setIndex(int index) {
+        this.index = index;
+    }
+
+    public Context getContext() {
+        return context;
+    }
+
+    public ArrayList<String> getList() {
+        return list;
+    }
+}
