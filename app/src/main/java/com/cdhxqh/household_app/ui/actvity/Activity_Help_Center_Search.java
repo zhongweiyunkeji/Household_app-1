8e923651272112f7490a8f6ad7f7a2a68ec9a70d
hexian <1336125942@qq.com>
2015-08-17 15:57:52 +0800

    测试

--- app/src/main/java/com/cdhxqh/household_app/ui/actvity/Activity_Help_Center_Search.java
+++ app/src/main/java/com/cdhxqh/household_app/ui/actvity/Activity_Help_Center_Search.java
@@ -1,26 +1,22 @@
 package com.cdhxqh.household_app.ui.actvity;
 
 import android.content.Intent;
-import android.graphics.Color;
-import android.graphics.drawable.GradientDrawable;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.text.Editable;
 import android.text.TextWatcher;
-import android.view.KeyEvent;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.EditText;
 import android.widget.ExpandableListView;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
-
 import com.cdhxqh.household_app.R;
 import com.cdhxqh.household_app.ui.adapter.HelpCenterExpandableListViewAdapter;
 import com.cdhxqh.household_app.ui.fragment.HelpCenterFragement;
 import com.cdhxqh.household_app.utils.UIUtils;
-
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -36,7 +32,11 @@ public class Activity_Help_Center_Search extends  BaseActivity {
     SwipeRefreshLayout refreshLayout;  // 刷新控件
     ExpandableListView listView;         // 搜索内容展现控件
     HelpCenterExpandableListViewAdapter adapter;
-    boolean onoff = false;
+    public boolean onoff = false;
+    ImageView backImg;
+    ImageView clearImg;
+    private int currentPage = 1;
+    private int showPage = 10;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,7 +44,15 @@ public class Activity_Help_Center_Search extends  BaseActivity {
         setContentView(R.layout.activity_help_center_search);
         findViewById();
         initView();
-        // onoff = ?;
+        currentPage = 1;
+        getData();
+    }
+
+    public void getData(){
+       Bundle bundle = getIntent().getExtras();
+        if(bundle!=null){
+            onoff = bundle.getBoolean("FLAG");
+        }
     }
 
     public void findViewById(){
@@ -52,34 +60,65 @@ public class Activity_Help_Center_Search extends  BaseActivity {
         hintArea = (LinearLayout)findViewById(R.id.scenic_search_result_empty);
         refreshLayout = (SwipeRefreshLayout)findViewById(R.id.scenic_refresh_container);
         listView = (ExpandableListView)findViewById(R.id.expan_search_listview);
+        backImg = (ImageView)findViewById(R.id.back_imageview_id);
+        clearImg = (ImageView)findViewById(R.id.title_search_id);
     }
 
     public void initView(){
         UIUtils.drawableRadiusTextView(srarchTextView, this.getResources().getColor(R.color.search_textview_background));  // 画出圆角
         hintArea.setVisibility(View.GONE);   // 初始化时不显示
         initExpandableListView();
+        clearImg.setVisibility(View.GONE);
 
         refreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() { // 注册下拉刷新按钮事件
             @Override
             public void onRefresh() {
-
-            }
-        });
-
-        srarchTextView.addTextChangedListener(new TextWatcher(){
-            @Override
-            public void afterTextChanged(Editable s) {
+                currentPage++;
                 ArrayList<String> group = new ArrayList<String>(0);
                 Map<String, List<String>> child = new HashMap<String, List<String>>(0);
                 for (int i = 0; i < srarchTextView.getText().length(); i++) {
-                    group.add("AAAA" + i);
+                    group.add("BBBB" + i);
                     ArrayList<String> temp = new ArrayList<String>(0);
                     for (int k = 0; k < 2; k++) {
-                        temp.add("AAAAAAAA" + i + k);
+                        temp.add("BBBBBBB" + i + k);
                     }
                     child.put(group.get(i), temp);
                 }
                 adapter.reloadDate(group, child);
+                refreshLayout.setRefreshing(false);// 关闭刷新事件
+            }
+        });
+
+        srarchTextView.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void afterTextChanged(Editable s) {
+                // refreshLayout.setRefreshing(true);  // 显示刷新控件
+                currentPage = 1;
+                if(s.length()!=0){
+                    clearImg.setVisibility(View.VISIBLE);
+                }
+                Handler handler = new Handler() {
+                    public void handleMessage(Message msg) {
+                        ArrayList<String> group = new ArrayList<String>(0);
+                        Map<String, List<String>> child = new HashMap<String, List<String>>(0);
+                        for (int i = 0; i < 100; i++) {
+                            group.add("AAAA" + i);
+                            ArrayList<String> temp = new ArrayList<String>(0);
+                            for (int k = 0; k < 100; k++) {
+                                temp.add("AAAAAAAA" + i + k);
+                            }
+                            child.put(group.get(i), temp);
+                        }
+                        adapter.reloadDate(group, child);
+                        listView.setGroupIndicator(null);  // 去掉展开和收缩箭头
+                        updateExpandList(group, child);      // 展开子项
+
+                        // refreshLayout.setRefreshing(false);// 关闭刷新事件
+                    }
+                };
+
+                handler.sendEmptyMessage(10000);
+
             }
 
             @Override
@@ -90,6 +129,22 @@ public class Activity_Help_Center_Search extends  BaseActivity {
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
 
+            }
+        });
+
+        backImg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+                ;
+            }
+        });
+
+        clearImg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                srarchTextView.setText("");
+                clearImg.setVisibility(View.GONE);
             }
         });
     }
@@ -116,6 +171,16 @@ public class Activity_Help_Center_Search extends  BaseActivity {
             }
         });
         listView.setAdapter(adapter);
+    }
+
+    public void updateExpandList(ArrayList<String> group, Map<String, List<String>> child){
+        if(group==null || child==null){
+            throw new RuntimeException("组和子元素都不能为空");
+        }
+        adapter.update(group, child);
+        for (int i = 0; i < adapter.getGroup().size(); i++) {  // 需要调用才方法才能默认展开ExpandableListView
+            listView.expandGroup(i);
+        }
     }
 
 }
