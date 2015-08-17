cc2fb060496eccb9e4f0f8fc1dfb5693da2b97b6
wxs19920926 <425767281@qq.com>
2015-08-17 17:21:14 +0800

    aa

--- app/src/main/java/com/cdhxqh/household_app/ui/actvity/AddContacterActivity.java
+++ app/src/main/java/com/cdhxqh/household_app/ui/actvity/AddContacterActivity.java
@@ -1,8 +1,11 @@
 package com.cdhxqh.household_app.ui.actvity;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -10,6 +13,10 @@ import android.widget.TextView;
 
 import com.cdhxqh.household_app.R;
 import com.cdhxqh.household_app.model.Contacters;
+import com.cdhxqh.household_app.ui.action.impl.OnItemClickCallBackImpl;
+import com.cdhxqh.household_app.ui.fragment.CommonContactFragment;
+
+import java.io.Serializable;
 
 /**
  * Created by Administrator on 2015/8/15.
@@ -21,97 +28,161 @@ public class AddContacterActivity extends BaseActivity {
     TextView name;
 
     /**
-     *填写手机号
+     * 填写手机号
      */
     TextView phone;
 
     /**
-     *分组
+     * 分组
      */
     ImageView select_group;
 
     /**
-     *提交
+     * 提交
      */
     Button commit_id;
 
     /**
-     *对象
+     * 对象
      */
     Contacters contacts;
 
     TextView role_group;
 
+    /**
+     * 编辑
+     */
+    String judje;
+
+    CommonContactFragment commonContactFragment;
+
     private static final int PICK_CONTACT = 1;
 
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_add_contacter);
-            findViewById();
-            getData();
-            initView();
-        }
+    public static final int ACTIVITY_REGISTRY_RESPONSE1 = 0;
 
-        protected void findViewById() {
-            /**
-             * name
-             */
-            name = (TextView) findViewById(R.id.name);
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_contacter);
+        findViewById();
+        getData();
+        initView();
+    }
 
-            /**
-             * phone
-             */
-            phone = (TextView) findViewById(R.id.phone);
+    protected void findViewById() {
+        /**
+         * name
+         */
+        name = (TextView) findViewById(R.id.name);
 
-            /**
-             * 分组
-             */
-            select_group = (ImageView) findViewById(R.id.select_group);
-            /**
-             * 添加联系人
-             */
-            commit_id = (Button) findViewById(R.id.commit_id);
-            /**
-             * 分组显示
-             */
-            role_group = (TextView) findViewById(R.id.role_group);
-        }
-
-
-        protected void initView() {
-            name.setText(contacts.getName());
-            phone.setText(contacts.getPhone());
-            role_group.setText(contacts.getType());
-            select_group.setOnClickListener(selectGroupOnClickListener);
-//            commit_id.setOnClickListener(commitOnClickListener);
-        }
+        /**
+         * phone
+         */
+        phone = (TextView) findViewById(R.id.phone);
 
         /**
          * 分组
          */
-        final Activity activity = this;
-        private View.OnClickListener selectGroupOnClickListener = new View.OnClickListener(){
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(activity, Activity_Role_Group.class);
-                startActivityForResult(intent, PICK_CONTACT);
-                }
-            };
-
+        select_group = (ImageView) findViewById(R.id.select_group);
         /**
-         * 提交
+         * 添加联系人
          */
-        private View.OnClickListener commitOnClickListener = new View.OnClickListener(){
-            @Override
-            public void onClick(View v) {
+        commit_id = (Button) findViewById(R.id.commit_id);
+        /**
+         * 分组显示
+         */
+        role_group = (TextView) findViewById(R.id.role_group);
+    }
 
+
+    protected void initView() {
+        if (contacts != null) {
+            name.setText(contacts.getName());
+            phone.setText(contacts.getPhone());
+            role_group.setText(contacts.getType());
+        }
+        select_group.setOnClickListener(selectGroupOnClickListener);
+        commit_id.setOnClickListener(commitOnClickListener);
+    }
+
+    /**
+     * 分组
+     */
+    final Activity activity = this;
+    private View.OnClickListener selectGroupOnClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent intent = new Intent(activity, Activity_User_Type.class);
+            startActivityForResult(intent, PICK_CONTACT);
+        }
+    };
+
+    /**
+     * 提交
+     */
+    private View.OnClickListener commitOnClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Contacters contacters = new Contacters();
+            if (name.getText() != null && !name.getText().toString().trim().equals("") && phone.getText() != null && !phone.getText().toString().trim().equals("")) {
+                if (role_group.getText() != null) {
+                    contacters.setName(name.getText().toString());
+                    contacters.setPhone(phone.getText().toString());
+                    contacters.setType(role_group.getText().toString());
+                }
+                if (judje != null) {
+                    if (!judje.equals("edit")) {
+                        Intent intent = new Intent();
+                        intent.putExtra("contactList", (Serializable) contacters);
+                        setResult(RESULT_OK, intent);
+                        finish();
+                    }else {
+                        Intent intent = new Intent();
+                        intent.putExtra("contactList", (Serializable) contacters);
+                        setResult(RESULT_OK, intent);
+                        finish();
+                    }
+                }
+            } else {
+                if (name.getText().toString().trim().equals("")) {
+                    new AlertDialog.Builder(AddContacterActivity.this).setTitle("标题").setMessage("请填写姓名").setPositiveButton("确定", null).show();
+                } else if (phone.getText().toString().trim().equals("")) {
+                    new AlertDialog.Builder(AddContacterActivity.this).setTitle("标题").setMessage("请填写手机号").setPositiveButton("确定", null).show();
+                }
             }
-        };
+
+        }
+    };
 
     /**
      * 获取数据
      */
     private void getData() {
         contacts = (Contacters) getIntent().getSerializableExtra("contactList");
+        judje = getIntent().getStringExtra("edit");
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if ((OnItemClickCallBackImpl.ACTIVITY_REGISTRY_RESPONSE1 == resultCode) && (Activity_Registry_User.ACTIVITY_REGISTRY_REQUEST1 == requestCode)) {
+            Bundle bundle = data.getExtras();
+            String text = bundle.getString("text");
+            role_group.setText(text);
+            Log.i("", "----------------------->" + text);
+        }
+    }
+
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+
+        if (keyCode == KeyEvent.KEYCODE_BACK
+                && event.getRepeatCount() == 0) {
+            if (contacts != null) {
+                Intent intent = new Intent();
+                intent.putExtra("contactList", (Serializable) contacts);
+                setResult(RESULT_OK, intent);
+            }
+            finish();
+        }
+        return super.onKeyDown(keyCode, event);
     }
 }
