a1f371ab6b63b8b36972d485da11a7de88bf1b4d
hexian <1336125942@qq.com>
2015-08-18 11:07:42 +0800

    测试

---
+++ app/src/main/java/com/cdhxqh/household_app/ui/action/AlarmOnClickCallBack.java
@@ -0,0 +1,14 @@
+package com.cdhxqh.household_app.ui.action;
+
+import android.view.View;
+
+import com.cdhxqh.household_app.model.Alarm;
+
+/**
+ * Created by hexian on 2015/8/17.
+ */
+public interface AlarmOnClickCallBack {
+
+    public void onClick(int position, View convertView, Alarm alarm);
+
+}
