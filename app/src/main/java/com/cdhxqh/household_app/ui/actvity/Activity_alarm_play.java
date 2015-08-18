528acfe81a4e229fef774e3f6a5f92d1633ebd1a
hexian <1336125942@qq.com>
2015-08-18 19:12:33 +0800

    测试

---
+++ app/src/main/java/com/cdhxqh/household_app/ui/actvity/Activity_alarm_play.java
@@ -0,0 +1,27 @@
+package com.cdhxqh.household_app.ui.actvity;
+
+import android.os.Bundle;
+
+import com.cdhxqh.household_app.R;
+
+/**
+ * Created by hexian on 2015/8/18.
+ */
+public class Activity_alarm_play extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.acivity_alarm_play);
+        findViewById();
+        initView();
+    }
+
+    public void findViewById(){
+
+    }
+
+    public void initView(){
+
+    }
+}
