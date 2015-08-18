513988b9388d7f476686d396e3ab9730b23794b7
hexian <1336125942@qq.com>
2015-08-18 19:28:44 +0800

    测试

--- app/src/main/java/com/cdhxqh/household_app/ui/actvity/Activity_alarm_play.java
+++ app/src/main/java/com/cdhxqh/household_app/ui/actvity/Activity_alarm_play.java
@@ -1,6 +1,10 @@
 package com.cdhxqh.household_app.ui.actvity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.VideoView;
 
 import com.cdhxqh.household_app.R;
 
@@ -8,6 +12,16 @@ import com.cdhxqh.household_app.R;
  * Created by hexian on 2015/8/18.
  */
 public class Activity_alarm_play extends BaseActivity {
+
+    ImageView backImg;
+    TextView titleText;
+    ImageView titleImg;
+
+    VideoView video;
+    TextView qeuipName;
+    TextView qeuipAddr;
+    TextView alarmType;
+    TextView alarmContent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,9 +33,25 @@ public class Activity_alarm_play extends BaseActivity {
 
     public void findViewById(){
 
+        backImg = (ImageView)findViewById(R.id.back_imageview_id);
+        titleImg = (ImageView)findViewById(R.id.title_add_id);
+        titleText = (TextView)findViewById(R.id.title_text_id);
+
+        video = (VideoView)findViewById(R.id.video_paly);
+        qeuipName = (TextView)findViewById(R.id.equip_name);
+        qeuipAddr = (TextView)findViewById(R.id.equip_addr);
+        alarmType = (TextView)findViewById(R.id.alarm_type);
+        alarmContent = (TextView)findViewById(R.id.alarm_content);
     }
 
     public void initView(){
-
+        titleImg.setVisibility(View.GONE);
+        titleText.setText("±¨¾¯»Ø·Å");
+        backImg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
