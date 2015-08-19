f1f05b5bb7f4fb02e0aede4a8c5a1ce7b155d805
wxs19920926 <425767281@qq.com>
2015-08-19 09:38:23 +0800

    aa

--- app/src/main/java/com/cdhxqh/household_app/ui/fragment/SettingFragment.java
+++ app/src/main/java/com/cdhxqh/household_app/ui/fragment/SettingFragment.java
@@ -1,7 +1,9 @@
 package com.cdhxqh.household_app.ui.fragment;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,32 +11,34 @@ import android.widget.RelativeLayout;
 
 import com.cdhxqh.household_app.R;
 import com.cdhxqh.household_app.ui.widget.DataCleanManager;
+import com.cdhxqh.household_app.ui.widget.NetWorkUtil;
 
 import java.util.ArrayList;
 
 /**
  * Created by Administrator on 2015/8/18.
  */
-public class SettingFragment extends BaseFragment{
+public class SettingFragment extends BaseFragment {
     /**
-     * 省流量
+     * 2G或3G
      */
     private RelativeLayout install_flow;
 
     /**
-     *清除缓存
+     * 清除缓存
      */
     private RelativeLayout catchs;
 
-    /**
-     * 清除缓存
-     */
-    DataCleanManager dataCleanManager;
+    Activity activity;
+
+//    /**
+//     * 清除缓存
+//     */
+//    DataCleanManager dataCleanManager;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState)
-    {
+                             Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.activity_my_settings, container, false);
         findViewById(view);
         initView();
@@ -42,25 +46,41 @@ public class SettingFragment extends BaseFragment{
     }
 
     protected void findViewById(View view) {
-        install_flow = (RelativeLayout)view.findViewById(R.id.install_flow);
-        catchs = (RelativeLayout)view.findViewById(R.id.catchs);
+        install_flow = (RelativeLayout) view.findViewById(R.id.install_flow);
+        catchs = (RelativeLayout) view.findViewById(R.id.catchs);
     }
 
     protected void initView() {
-//        install_flow.setOnClickListener(installFlowOnClickListener);
+        install_flow.setOnClickListener(installFlowOnClickListener);
         catchs.setOnClickListener(catchsOnClickListener);
+
+        activity = this.getActivity();
     }
 
     /**
      * 清除缓存
      */
-    final Activity activity = this.getActivity();
     private View.OnClickListener catchsOnClickListener = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
-//            DataCleanManager.cleanInternalCache(activity);//清除本应用内部缓存
-//            DataCleanManager.cleanDatabases(activity);//清除本应用所有数据库
-//            DataCleanManager.cleanSharedPreference(activity);//清除本应用SharedPreference
+//            DataCleanManager.cleanInternalCache(activity);
+//            DataCleanManager.cleanDatabases(activity);
+//            DataCleanManager.cleanSharedPreference(activity);
+        }
+    };
+
+    /**
+     * 2G或3G模式
+     */
+    private View.OnClickListener installFlowOnClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            String type = NetWorkUtil.getCurrentNetworkType(activity);
+            if (type.equals("Wi-Fi") || type.equals("4G")) {
+                new AlertDialog.Builder(activity).setMessage("wife网络环境").setPositiveButton("确定", null).show();
+            } else if (type.equals("2G") || type.equals("3G")) {
+                new AlertDialog.Builder(activity).setMessage("打开后非wife网络环境下将不显示图片喔，确定打开吗？").setPositiveButton("确定", null).show();
+            }
         }
     };
 
