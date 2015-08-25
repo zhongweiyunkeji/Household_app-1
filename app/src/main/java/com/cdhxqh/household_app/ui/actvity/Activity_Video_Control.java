b39fa00a533a85e365679c3448c062aa0d9342e4
hexian <1336125942@qq.com>
2015-08-25 18:12:31 +0800

    添加等待对话框

--- app/src/main/java/com/cdhxqh/household_app/ui/actvity/Activity_Video_Control.java
+++ app/src/main/java/com/cdhxqh/household_app/ui/actvity/Activity_Video_Control.java
@@ -1,13 +1,20 @@
 package com.cdhxqh.household_app.ui.actvity;
 
+import android.annotation.TargetApi;
 import android.app.Activity;
 import android.graphics.Color;
 import android.graphics.drawable.Drawable;
 import android.media.MediaPlayer;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.text.Html;
+import android.util.Log;
 import android.view.Gravity;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.MediaController;
 import android.widget.ScrollView;
@@ -18,6 +25,11 @@ import com.cdhxqh.household_app.R;
 
 import java.io.InputStream;
 import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+import java.util.Timer;
+import java.util.TimerTask;
 
 /**
  * Created by hexian on 2015/8/10.
@@ -30,11 +42,25 @@ public class Activity_Video_Control extends BaseActivity {
     LinearLayout layout;
     ScrollView   scrollView;
     VideoView videoView;
+    ImageView imgView;
+
+    Map<String, String> urls = new HashMap<String, String>(0);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_video_control);
+        urls.put("1", "rtsp://211.139.194.251:554/live/2/13E6330A31193128/5iLd2iNl5nQ2s8r8.sdp");
+        urls.put("2", "http://ws.jia.360.cn/qhwljia/36020187375/index.m3u8");
+        urls.put("3", "http://vshare.ys7.com:80/hcnp/497827719_1_2_1_0_183.136.184.7_6500.m3u8");
+        urls.put("4", "http://vshare.ys7.com:80/hcnp/517678458_1_1_1_0_183.136.184.7_6500.m3u8");
+        urls.put("5", "http://vshare.ys7.com:80/hcnp/516427804_3_2_1_0_183.136.184.7_6500.m3u8");
+        urls.put("6", "http://ws.jia.360.cn/qhwljia/36020166912/index.m3u8");
+        urls.put("7", "http://vshare.ys7.com:80/hcnp/513351987_1_2_1_0_183.136.184.7_6500.m3u8");
+        urls.put("8", "http://vshare.ys7.com:80/hcnp/500894371_1_2_1_0_183.136.184.7_6500.m3u8");
+        urls.put("9", "http://vshare.ys7.com:80/hcnp/497827719_1_2_1_0_183.136.184.7_6500.m3u8");
+
+
         findViewById();
         initView();
     }
@@ -43,31 +69,56 @@ public class Activity_Video_Control extends BaseActivity {
         videoView = (VideoView)findViewById(R.id.video_m3u8);
         layout = (LinearLayout)findViewById(R.id.video_monitoring_layout);
         scrollView = (ScrollView)findViewById(R.id.scrollView);
+        imgView = (ImageView)findViewById(R.id.buffer);
         pay();
         // linearLayout = (FixGridLayout)findViewById(R.id.linearLayout);
     }
 
     public void pay(){
+        Random random1 = new Random(9);
+        int x = random1.nextInt();
+        if(x<0){
+            x = (-x)% 10;
+        }
+        if(x == 0){
+            x = 3;
+        }
         // http://vshare.ys7.com:80/hcnp/497827719_1_2_1_0_183.136.184.7_6500.m3u8
-        // rtsp://v2.cache2.c.youtube.com/CjgLENy73wIaLwm3JbT_%ED%AF%80%ED%B0%819HqWohMYESARFEIJbXYtZ29vZ2xlSARSB3Jlc3VsdHNg_vSmsbeSyd5JDA==/0/0/0/video.3gp
-        // rtsp://183.136.184.33:8554/demo://520752318:1:1:1:0:183.136.184.7:6500
-        final Uri uri = Uri.parse("rtsp://211.139.194.251:554/live/2/13E6330A31193128/5iLd2iNl5nQ2s8r8.sdp");
-        videoView.setMediaController(new MediaController(this));
+        // rtsp://211.139.194.251:554/live/2/13E6330A31193128/5iLd2iNl5nQ2s8r8.sdp
+        final Uri uri = Uri.parse("http://vshare.ys7.com:80/hcnp/513351987_1_2_1_0_183.136.184.7_6500.m3u8");
+        // videoView.setMediaController(new MediaController(this));
         videoView.setVideoURI(uri);
         videoView.requestFocus();
         videoView.start();
+        videoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mp) {
+                imgView.setVisibility(View.GONE);
+            }
+        });
+
 
         videoView.setOnErrorListener(new MediaPlayer.OnErrorListener() {
             @Override
             public boolean onError(MediaPlayer mp, int what, int extra) {
-                videoView.stopPlayback(); // 首先停止视频播放
+                if (!videoView.isPlaying()) {
+                    imgView.setVisibility(View.VISIBLE);
+                }
+                videoView.stopPlayback();    // 首先停止视频播放
                 videoView.setVideoURI(uri);  // 设置播放地址
-                videoView.requestFocus();  // 获取请求码
-                videoView.start();        // 开始播放
-                return true;//如果设置true就可以防止他弹出错误的提示框！
+                videoView.requestFocus();    // 获取请求码
+                videoView.start();          // 开始播放
+                return true;                //如果设置true就可以防止他弹出错误的提示框！
             }
         });
 
+    }
+
+    @Override
+    protected void onResume() {  // 继续播放
+        imgView.setVisibility(View.VISIBLE);
+        pay();
+        super.onResume();
     }
 
     @Override
