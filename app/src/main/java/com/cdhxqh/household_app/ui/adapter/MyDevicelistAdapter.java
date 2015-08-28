fc6110fc949ced92c01e7eef349a4da166f93dab
hexian <1336125942@qq.com>
2015-08-28 19:49:07 +0800

    增加萤石SDK

--- app/src/main/java/com/cdhxqh/household_app/ui/adapter/MyDevicelistAdapter.java
+++ app/src/main/java/com/cdhxqh/household_app/ui/adapter/MyDevicelistAdapter.java
@@ -3,6 +3,9 @@ package com.cdhxqh.household_app.ui.adapter;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
@@ -17,6 +20,7 @@ import com.cdhxqh.household_app.R;
 import com.cdhxqh.household_app.model.MyDevice;
 import com.cdhxqh.household_app.ui.actvity.Activity_Video_Control;
 import com.cdhxqh.household_app.ui.actvity.TestActivity;
+import com.videogo.openapi.bean.resp.CameraInfo;
 
 import java.util.ArrayList;
 
@@ -27,11 +31,23 @@ public class MyDevicelistAdapter extends RecyclerView.Adapter<MyDevicelistAdapte
     private static final String TAG = "ReviewListAdapter";
     Context mContext;
 
-    ArrayList<MyDevice> list=new ArrayList<MyDevice>();
+    ArrayList<CameraInfo> list=new ArrayList<CameraInfo>();
 
     public MyDevicelistAdapter(Context context){
         this.mContext = context;
     }
+
+    private Bitmap getBitMap(int resid){
+        BitmapFactory.Options opts = new BitmapFactory.Options();
+        Bitmap mBitmap = BitmapFactory.decodeResource(mContext.getResources(),resid, opts);
+        // 缩放图片
+        Matrix matrix = new Matrix();
+        matrix.postScale(0.15F, 0.15F);
+        mBitmap = Bitmap.createBitmap(mBitmap, 0, 0, mBitmap.getWidth(), mBitmap.getHeight(), matrix, true);
+
+        return mBitmap;
+    }
+
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.activity_mydevice_adapter, parent, false);
@@ -40,16 +56,42 @@ public class MyDevicelistAdapter extends RecyclerView.Adapter<MyDevicelistAdapte
 
     @Override
     public void onBindViewHolder(ViewHolder holder, final int position) {
-        holder.nameView.setText(list.get(position).getName());
-        holder.sizeView.setText(list.get(position).getSize()+"");
-        holder.placeView.setText(list.get(position).getPlace());
-        holder.numberView.setText(list.get(position).getNumber());
+        final CameraInfo info  = list.get(position);
+        holder.nameView.setText(list.get(position).getCameraName());  // 摄像头名称
+        holder.sizeView.setText(0+"");
+        String name = "";
+        int imgResid = 0;
+        if(0 == position){
+            name = "办公室";
+            imgResid = R.drawable.camera_office;
+        } else
+        if(1 == position){
+            name = "楼道";
+            imgResid = R.drawable.camera_corridor;
+        } else
+        if(2 == position){
+            name = "操场";
+            imgResid = R.drawable.camera_playground;
+        }
+
+        holder.iconView.setImageBitmap(getBitMap(imgResid));
+        holder.placeView.setText(name);
+        holder.numberView.setText(info.getDeviceSerial());
+        String status = (info.getStatus() == 1) ? "在线" : "离线";
+        holder.status.setText(status);
+        if("在线".equals(status)){
+            holder.status.setTextColor(mContext.getResources().getColor(R.color.green));
+        } else
+        if("离线".equals(status)){
+            holder.status.setTextColor(mContext.getResources().getColor(R.color.grgray));
+        }
+
         holder.linearLayout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent();
                 Bundle bundle = new Bundle();
-                bundle.putString("device_name", list.get(position).getName());
+                bundle.putParcelable("device_name", info);
                 intent.putExtras(bundle);
                 intent.setClass(mContext,Activity_Video_Control.class);
                 mContext.startActivity(intent);
@@ -76,6 +118,7 @@ public class MyDevicelistAdapter extends RecyclerView.Adapter<MyDevicelistAdapte
         public TextView placeView;
         /**编号**/
         public TextView numberView;
+        public TextView status;
         public ViewHolder(final View itemView) {
             super(itemView);
             linearLayout = (LinearLayout) itemView.findViewById(R.id.mydevice_content_id);
@@ -84,14 +127,15 @@ public class MyDevicelistAdapter extends RecyclerView.Adapter<MyDevicelistAdapte
             nameView = (TextView) itemView.findViewById(R.id.mydevice_name);
             placeView = (TextView) itemView.findViewById(R.id.mydevice_place);
             numberView = (TextView) itemView.findViewById(R.id.mydevice_number);
+            status = (TextView) itemView.findViewById(R.id.mydevice_open);
         }
     }
 
-    public void update(ArrayList<MyDevice> data, boolean merge) {
+    public void update(ArrayList<CameraInfo> data, boolean merge) {
         Log.i(TAG, "mItems=" + list.size());
         if (merge && list.size() > 0) {
             for (int i = 0; i < list.size(); i++) {
-                MyDevice device = list.get(i);
+                CameraInfo device = list.get(i);
                 boolean exist = false;
                 for (int j = 0; j < data.size(); j++) {
                     if (data.get(j) == device) {
