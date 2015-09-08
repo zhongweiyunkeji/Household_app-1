0a2d6614d6f40bfa2e05a935cd13490c7766e40c
wxs19920926 <425767281@qq.com>
2015-09-08 19:57:32 +0800

    aa

---
+++ app/src/main/java/com/cdhxqh/household_app/ui/actvity/DealInformationActivity.java
@@ -0,0 +1,25 @@
+package com.cdhxqh.household_app.ui.actvity;
+
+import android.os.Bundle;
+
+import com.cdhxqh.household_app.R;
+
+/**
+ * Created by Administrator on 2015/9/8.
+ */
+public class DealInformationActivity extends BaseActivity{
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_deal_information);
+        findViewById();
+        initView();
+    }
+
+    public void findViewById() {
+
+    }
+
+    public void initView() {
+
+    }
+}
