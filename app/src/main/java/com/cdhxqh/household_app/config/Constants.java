a3e59afa29ba8ea69fb64ae856e062eddf5d29a1
hexian <1336125942@qq.com>
2015-09-16 15:37:00 +0800

    增加版本相关属性

--- app/src/main/java/com/cdhxqh/household_app/config/Constants.java
+++ app/src/main/java/com/cdhxqh/household_app/config/Constants.java
@@ -24,8 +24,8 @@ public class Constants {
     /**视频截图保存路径**/
     public static final String VIDEO_PATH="/storage/sdcard1/";
 
-    // 182.92.158.158   192.168.1.101
-    public static final String BASE_URL = "http://182.92.158.158:8080/security/";
+    // 182.92.158.158   192.168.1.101   192.168.1.97
+    public static final String BASE_URL = "http://192.168.1.97:8080/security/";
 
     // 设备列表
     public static final String DEVICE_LIST = BASE_URL + "yscamera/list";
